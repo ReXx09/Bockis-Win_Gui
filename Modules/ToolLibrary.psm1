021014ee380323d4edcb2d2f1108a60a32015f0d
ReXx09 <rexxlab@tutamail.com>
2026-08-25 19:32:46 +0200

    Keine Änderungen am Code vorgenommen

--- Modules/ToolLibrary.psm1
+++ Modules/ToolLibrary.psm1
@@ -272,6 +272,24 @@ $script:toolLibrary['applications'] = @(
         Winget      = 'Valve.Steam'
     },
     @{
+        Name        = 'Epic Games Launcher'
+        Description = 'Gaming-Plattform von Epic Games zum Verwalten und Spielen von Spielen (Fortnite, Unreal Engine u.a.)'
+        Version     = 'Aktuell'
+        DownloadUrl = 'https://store.epicgames.com/'
+        Category    = 'Anwendungen'
+        Tags        = @('Gaming', 'Games', 'Store', 'Epic', 'Launcher')
+        Winget      = 'EpicGames.EpicGamesLauncher'
+    },
+    @{
+        Name        = 'MediaHuman MP3 Converter'
+        Description = 'Kostenloser Audio-Konverter zum Umwandeln von Musikdateien in MP3, AAC, WMA, FLAC, OGG u.v.m.'
+        Version     = 'Aktuell'
+        DownloadUrl = 'https://www.mediahuman.com/mp3-converter/'
+        Category    = 'Anwendungen'
+        Tags        = @('Audio', 'Converter', 'MP3', 'Music', 'Media')
+        Winget      = 'MediaHuman.MP3Converter'
+    },
+    @{
         Name        = 'Total Commander'
         Description = 'Leistungsstarker Dateimanager mit Zwei-Panel-Ansicht, FTP-Client und Archiv-Unterstützung'
         Version     = 'Aktuell'
